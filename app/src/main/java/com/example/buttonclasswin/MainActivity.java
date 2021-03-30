89fb4c86db2af344beeafbcc9b0774fd4c0ab7e7
ccccljs <cho38suk@hamail.net>
2021-03-30 18:08:44 +0900

    Listener class in a seperate file

--- app/src/main/java/com/example/buttonclasswin/MainActivity.java
+++ app/src/main/java/com/example/buttonclasswin/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    Button mButton1 , mButton2;
+    Button mButton1; // mButton2;
     TextView mTextView1;
 
     @Override
@@ -17,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton1=findViewById(R.id.button1);
-        mButton2=findViewById(R.id.button2);
+       // mButton2=findViewById(R.id.button2);
         mTextView1=findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(new MyOnClickListener());
-        mButton2.setOnClickListener(new MyOnClickListener2());
+       // mButton2.setOnClickListener(new MyOnClickListener2());
     }
 
     private class MyOnClickListener implements View.OnClickListener {
@@ -31,11 +31,13 @@ public class MainActivity extends AppCompatActivity {
             mTextView1.setText("You clicked 이준석 's  button");
         }
     }
-
+/*
     private class MyOnClickListener2 implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             mTextView1.setText("You Clicked my 2nd button");
         }
     }
+
+ */
 }
